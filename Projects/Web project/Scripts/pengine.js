67e70730af6c0d47d012a93e392d4d2588aa307e
jose1103-lab <josealbertobritosantana@gmail.com>
2024-11-19 23:03:02 +0000

    Done with the add

--- Projects/Web project/Scripts/pengine.js
+++ Projects/Web project/Scripts/pengine.js
@@ -1,26 +1,5 @@
 //my first steps un javascript
 
- function suma(value1, value2){
-     let result = value1 + value2;
-     return result;
-    }
- function sustraction(value1, value2){
-     let result = value1 - value2;
-     return result;
-    }
- function multiplication(value1, value2){
-     let result = value1 * value2;
-     return result;
- }
- function division(value1, value2){
-     let result = value1 / value2;
-     return result;
- }
- function exponentiation(value1, value2){
-     let result = value1 ** value2;
-     return result;
- }
-
  while(true){
      let operator = prompt ("Opertion:\n A) Sum\n B) Subtraction\n C) Multiplication\n D) Division\n E) Exponentiation\n (F) Close");
      if(operator.toLowerCase() === "f"){break;}
@@ -28,28 +7,43 @@
      let value2 = parseFloat(prompt("Second digit:"));
      let display1 = document.getElementById("display");
      let result
+
+
+     function new_main_post(){
+        const new_para = document.createElement("p");
+        const main_node = document.getElementById("main_feed")
+        new_para.textContent = "New paragraph"
+        main_node.appendChild(new_para)
+     }
      
      switch(operator.toLowerCase()){
-         case "a":             
-            result = suma(value1, value2);
+         case "a": 
+            let suma = (a, b) => value1 + value2;
+            result = suma()
             break;    
-         case "b":
-            result = sustraction(value1, value2);
+        case "b":
+            let sustraction = (a, b) => value1 - value2;
+            result = sustraction()
             break;    
-         case "c":
-            result = multiplication(value1, value2);
-             break;    
-         case "d":
-             result = division(value1, value2);
-             break;    
-         case "e":
-             result = exponentiation(value1, value2);
+        case "c":
+            let multiplication = (a, b) => value1 * value2;
+            result = multiplication()
+            break;    
+        case "d":
+            let division = (a, b) => value1 / value2;
+            result = division()
+            break;    
+        case "e":
+             let exponentiation = (a, b) => value1 ** value2;
+             result = exponentiation()
              break;
-         default:
+        default:
             display1.style.fontSize = "16px";
             display1.style.color = "red";
             result = "Invalid operation ("+operator.toUpperCase()+")";
              
         }
         display1.textContent = result;
-}
+    }
+    
+    new_main_post();
